acad8777896a4bb19aad1658ecc8c3419e624b71
MaximilianoCoceres <maximilianococeres7@gmail.com>
2022-09-04 16:54:22 +0200

    update react

--- app.jsx
+++ app.jsx
@@ -18,7 +18,7 @@ class Clock extends React.Component {
     }
 
     componentWillUnmount() {
-        clearInterval(this - this.timerID);
+        clearInterval(this.timerID);
     }
 
     tick() {
@@ -38,7 +38,7 @@ class Clock extends React.Component {
 
 
 
-ReactDOM.render(<clock />, document.getElementById('reloj'));
+ReactDOM.render(<Clock />, document.getElementById('reloj'));
 
 
 
@@ -125,4 +125,34 @@ ReactDOM.render(
 
     />,
     document.getElementById('root')
+);
+
+
+
+class Toggle extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {isToggleOn:true};
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(prevState =>({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+    render(){
+        return(
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Toggle/>,
+    document.getElementById('boton')
 );
